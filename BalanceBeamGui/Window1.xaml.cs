b20d1ac786adfdce3ce721f62f12ce3b1e04cf86
Will Beasley <wibeasley@hotmail.com>
2013-10-15 14:39:21 -0500

    added brush instantiation

--- BalanceBeamGui/Window1.xaml.cs
+++ BalanceBeamGui/Window1.xaml.cs
@@ -63,19 +63,19 @@ namespace BalanceBeamGui {
         // private readonly Color[] _allPossibleColors = { Colors.AliceBlue, Colors.AntiqueWhite, Colors.Aqua, Colors.Aquamarine, Colors.Azure, Colors.Beige, Colors.Bisque, Colors.Black, Colors.BlanchedAlmond, Colors.Blue, Colors.BlueViolet, Colors.Brown, Colors.BurlyWood, Colors.CadetBlue, Colors.Chartreuse, Colors.Chocolate, Colors.Coral, Colors.CornflowerBlue, Colors.Cornsilk, Colors.Crimson, Colors.Cyan, Colors.DarkBlue, Colors.DarkCyan, Colors.DarkGoldenrod, Colors.DarkGray, Colors.DarkGreen, Colors.DarkKhaki, Colors.DarkMagenta, Colors.DarkOliveGreen, Colors.DarkOrange, Colors.DarkOrchid, Colors.DarkRed, Colors.DarkSalmon, Colors.DarkSeaGreen, Colors.DarkSlateBlue, Colors.DarkSlateGray, Colors.DarkTurquoise, Colors.DarkViolet, Colors.DeepPink, Colors.DeepSkyBlue, Colors.DimGray, Colors.DodgerBlue, Colors.Firebrick, Colors.FloralWhite, Colors.ForestGreen, Colors.Fuchsia, Colors.Gainsboro, Colors.GhostWhite, Colors.Gold, Colors.Goldenrod, Colors.Gray, Colors.Green, Colors.GreenYellow, Colors.Honeydew, Colors.HotPink, Colors.IndianRed, Colors.Indigo, Colors.Ivory, Colors.Khaki, Colors.Lavender, Colors.LavenderBlush, Colors.LawnGreen, Colors.LemonChiffon, Colors.LightBlue, Colors.LightCoral, Colors.LightCyan, Colors.LightGoldenrodYellow, Colors.LightGray, Colors.LightGreen, Colors.LightPink, Colors.LightSalmon, Colors.LightSeaGreen, Colors.LightSkyBlue, Colors.LightSlateGray, Colors.LightSteelBlue, Colors.LightYellow, Colors.Lime, Colors.LimeGreen, Colors.Linen, Colors.Magenta, Colors.Maroon, Colors.MediumAquamarine, Colors.MediumBlue, Colors.MediumOrchid, Colors.MediumPurple, Colors.MediumSeaGreen, Colors.MediumSlateBlue, Colors.MediumSpringGreen, Colors.MediumTurquoise, Colors.MediumVioletRed, Colors.MidnightBlue, Colors.MintCream, Colors.MistyRose, Colors.Moccasin, Colors.NavajoWhite, Colors.Navy, Colors.OldLace, Colors.Olive, Colors.OliveDrab, Colors.Orange, Colors.OrangeRed, Colors.Orchid, Colors.PaleGoldenrod, Colors.PaleGreen, Colors.PaleTurquoise, Colors.PaleVioletRed, Colors.PapayaWhip, Colors.PeachPuff, Colors.Peru, Colors.Pink, Colors.Plum, Colors.PowderBlue, Colors.Purple, Colors.Red, Colors.RosyBrown, Colors.RoyalBlue, Colors.SaddleBrown, Colors.Salmon, Colors.SandyBrown, Colors.SeaGreen, Colors.SeaShell, Colors.Sienna, Colors.Silver, Colors.SkyBlue, Colors.SlateBlue, Colors.SlateGray, Colors.Snow, Colors.SpringGreen, Colors.SteelBlue, Colors.Tan, Colors.Teal, Colors.Thistle, Colors.Tomato, Colors.Transparent, Colors.Turquoise, Colors.Violet, Colors.Wheat, Colors.White, Colors.WhiteSmoke, Colors.Yellow, Colors.YellowGreen };
         //Modifiable by user
         // default colors for comboBoxes - Jay 101513
-        // private SolidColorBrush _brushPalette = Color.FromArgb(255, 255, 255, 255); //white
-        // private SolidColorBrush _brushForegroundPositive = Color.FromArgb(255, 51, 160, 44); //green
-        // private SolidColorBrush _brushBackgroundPositive = Color.FromArgb(255, 166, 206, 227); //light blue
-        // private SolidColorBrush _brushForegroundNegative = Color.FromArgb(255, 227, 26, 28); // red
-        // private SolidColorBrush _brushBackgroundNegative = Color.FromArgb(255, 251, 154, 153); // pink
-        // private SolidColorBrush _brushHighlight = Color.FromArgb(255, 255, 127, 0); // orange
+        private SolidColorBrush _brushPalette = new SolidColorBrush(Color.FromArgb(255, 255, 255, 255)); //white
+        private SolidColorBrush _brushForegroundPositive = new SolidColorBrush(Color.FromArgb(255, 51, 160, 44)); //green
+        private SolidColorBrush _brushBackgroundPositive = new SolidColorBrush(Color.FromArgb(255, 166, 206, 227)); //light blue
+        private SolidColorBrush _brushForegroundNegative = new SolidColorBrush(Color.FromArgb(255, 227, 26, 28)); // red
+        private SolidColorBrush _brushBackgroundNegative = new SolidColorBrush(Color.FromArgb(255, 251, 154, 153)); // pink
+        private SolidColorBrush _brushHighlight =   new SolidColorBrush(Color.FromArgb(255, 255, 127, 0)); // orange
         
-		private SolidColorBrush _brushPalette = Brushes.MintCream;
-        private SolidColorBrush _brushForegroundPositive = Brushes.Blue;//
-        private SolidColorBrush _brushBackgroundPositive = Brushes.LightBlue;
-        private SolidColorBrush _brushForegroundNegative = Brushes.Firebrick;// Coral;
-        private SolidColorBrush _brushBackgroundNegative = Brushes.LightPink;// LightCoral;
-        private SolidColorBrush _brushHighlight = new SolidColorBrush(Color.FromArgb(119, 255, 255, 0));// (SolidColorBrush)(new BrushConverter().ConvertFromString("#77FFFF00"));//LightGoldenrodYellow; //#77FFFF00;//Brushes.Yellow;
+        //private SolidColorBrush _brushPalette = Brushes.MintCream;
+        //private SolidColorBrush _brushForegroundPositive = Brushes.Blue;//
+        //private SolidColorBrush _brushBackgroundPositive = Brushes.LightBlue;
+        //private SolidColorBrush _brushForegroundNegative = Brushes.Firebrick;// Coral;
+        //private SolidColorBrush _brushBackgroundNegative = Brushes.LightPink;// LightCoral;
+        //private SolidColorBrush _brushHighlight = new SolidColorBrush(Color.FromArgb(119, 255, 255, 0));// (SolidColorBrush)(new BrushConverter().ConvertFromString("#77FFFF00"));//LightGoldenrodYellow; //#77FFFF00;//Brushes.Yellow;
 
         //Fixed
         private SolidColorBrush _brushForegroundHeader = Brushes.Black;
